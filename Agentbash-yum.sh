c4e4ab434aff50949b05b21341faf6dcc6ed3d8c
root <root@shashank-pc>
2024-08-18 13:52:11 +0530

    required only pkgs

--- Agentbash-yum.sh
+++ Agentbash-yum.sh
@@ -28,32 +28,11 @@ pip --version
 echo "**********INSTALLING TAR**********"
 sudo yum install tar -y
 
-#echo "**********INSTALLING AWS-CLI**********"
-#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-#unzip awscliv2.zip
-#sudo ./aws/install
-#aws --version
-
-#echo "**********INSTALLING KUBECTL**********"
-#sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-#sudo chmod +x kubectl
-#sudo mv kubectl /usr/local/bin/
-#kubectl version --client
-
-
-#echo "**********INSTALLING EKSCTL**********"
-#curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-#sudo mv /tmp/eksctl /usr/local/bin
-#eksctl version
-
-echo "THIS SCRIPT INSTALLED UNZIP, CURL, GIT, DOCKER, PYTHON3, PIP, AWSCLI, KUBECTL, EKSCTL AND JAVA GIVEN AS -"
+echo "THIS SCRIPT INSTALLED UNZIP, CURL, GIT, DOCKER, PYTHON3, PIP AND JAVA GIVEN AS -"
 
 echo "JAVA VERSION $(java --version | awk 'NR==2 {print $NF}')"
 echo "PYTHON VERSION $(python3 --version | awk 'NR==1 {print $NF}')"
 echo "CURL $(curl --version | awk 'NR==1 {print $NF}')"
 echo "DOCKER $(docker --version | awk 'NR==1 {print $NF}')"
 echo "PIP $(pip --version | awk 'NR==1 {print $NF}')"
-#echo "AWS-CLI $(aws --version | awk 'NR==1 {print $NF}')"
-#echo "KUBECTL $(kubectl version --client | awk 'NR==1 {print $NF}')"
-#echo "EKSCTL $(eksctl version | awk 'NR==1 {print $NF}')"
 echo "TAR $(tar --version | awk 'NR==1 {print $NF}')"
